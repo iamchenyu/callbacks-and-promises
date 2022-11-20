f97f4fe2a570973195b990e853939264bf434e4f
Chenyu Wang <jadeyw7@gmail.com>
2022-11-20 12:39:11 -0500

    add async/await

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // const BASE_URL = "http://numbersapi.com";
 // Part 1
+// Promises
 // axios.get(`${BASE_URL}/random/year?json`).then((res) => console.log(res));
 
+// Async / Await
+// const fetchApiData = async () => {
+//   const { data } = await axios.get(`${BASE_URL}/random/trivia?json`);
+//   console.log(data.text);
+// };
+
 // Part 2
+// Promises
 // let factsPromises = [];
 
 // for (let i = 0; i < 4; i++) {
@@ -18,9 +26,18 @@
 //   })
 //   .catch((err) => console.log(err));
 
+// Async/Await
+// const MIN = 1;
+// const MAX = 3;
+
+// const fetchApiData = async () => {
+//   const { data } = await axios.get(`${BASE_URL}/${MIN}..${MAX}`);
+//   console.log(data);
+// };
+
 // Part 3
 // const NUM = 7;
-
+// Promises
 // axios
 //   .get(`${BASE_URL}/${NUM}/math?json`)
 //   .then((res) => {
@@ -44,9 +61,20 @@
 //   })
 //   .catch((err) => console.log(err));
 
+// Async/ Await
+// const fourPromises = Array(4).fill(axios.get(`${BASE_URL}/${NUM}?json`));
+// const fetchApiData = async () => {
+//   const res = await Promise.all(fourPromises);
+//   for (let re of res) {
+//     const li = $(`<li>${re.data.text}</li>`);
+//     li.appendTo($("#facts"));
+//   } // can also use array methods like forEach
+// };
+
 // const BASE_URL = "https://deckofcardsapi.com/api/deck/";
 
 // PART 1
+// Promises
 // axios
 //   .get(`${BASE_URL}/new/draw/?count=1`)
 //   .then((res) =>
@@ -54,7 +82,14 @@
 //   )
 //   .catch((err) => console.log(err));
 
+// Async/Await
+// const fetchApiData = async () => {
+//   const { data } = await axios.get(`${BASE_URL}/new/draw/?count=1`);
+//   console.log(`${data.cards[0].value} of ${data.cards[0].suit}`);
+// };
+
 // PART 2
+// Promises
 // axios
 //   .get(`${BASE_URL}/new/draw/?count=1`)
 //   .then((res) => {
@@ -71,7 +106,19 @@
 //   })
 //   .catch((err) => console.log(err));
 
+// Async/Await
+// const fetchApiData = async () => {
+//   const res = await axios.get(`${BASE_URL}/new/draw/?count=1`);
+//   const deckId = res.data.deck_id;
+//   const { data } = await axios.get(`${BASE_URL}/${deckId}/draw/?count=1`);
+//   console.log(
+//     `Card 1: ${res.data.cards[0].value} of ${res.data.cards[0].suit}`
+//   );
+//   console.log(`Card 2: ${data.cards[0].value} of ${data.cards[0].suit}`);
+// };
+
 // Part 3
+// Promises
 // let deckID;
 // let remaining;
 
@@ -108,10 +155,42 @@
 
 // cardButton.on("click", handleButtonClick);
 
-const BASE_URL = "https://pokeapi.co/api/v2";
-let threePokemonPromises = [];
-let threeSpeciesPromises = [];
+// Async/Await
+// let deck_id;
+// let remaining;
 
+// const getNewDeck = async () => {
+//   const { data } = await axios.get(`${BASE_URL}/new`);
+//   deck_id = data.deck_id;
+//   remaining = data.remaining;
+//   // shuffle cards
+//   await axios.get(`${BASE_URL}/${deck_id}/shuffle`);
+// };
+
+// const handleButtonClick = async () => {
+//   if (remaining > 0) {
+//     const { data } = await axios.get(`${BASE_URL}/${deck_id}/draw/?count=1`);
+//     $("#showcase").append($(`<img src=${data.cards[0].image} />`));
+//     remaining = data.remaining;
+//   } else {
+//     alert(
+//       "You've drawed every card in this deck. Please refresh the page for a new deck."
+//     );
+//     cardButton.remove();
+//   }
+// };
+
+// getNewDeck();
+
+// const cardButton = $("<button>DRAW A CARD!</button>");
+// $("#showcase").append(cardButton);
+// cardButton.on("click", handleButtonClick);
+
+const BASE_URL = "https://pokeapi.co/api/v2";
+// let threePokemonPromises = [];
+// let threeSpeciesPromises = [];
+
+// Promises
 // Option 1
 // Part 1
 // axios
@@ -147,29 +226,49 @@ let threeSpeciesPromises = [];
 
 // Option 2
 // Part 1
-axios
-  .get(`${BASE_URL}/pokemon?limit=100000&offset=0`)
-  .then((res) => {
-    const pokemonPromises = res.data.results;
-    const pokemonCounts = res.data.results.length;
-    // Part 2
-    for (let i = 0; i < 3; i++) {
-      let rand = Math.floor(Math.random() * pokemonCounts);
-      threePokemonPromises.push(axios.get(pokemonPromises[rand].url));
-    }
-    // Part 3
-    Promise.all(threePokemonPromises)
-      .then((res) => {
-        res.forEach((pokemon) => {
-          axios.get(pokemon.data.species.url).then((res) => {
-            let flavor_entry = res.data.flavor_text_entries.find(
-              (entry) => entry.language.name === "en"
-            );
-            let name = res.data.name;
-            console.log(`${name}: ${flavor_entry.flavor_text}`);
-          });
-        });
-      })
-      .catch((err) => console.log(err));
-  })
-  .catch((err) => console.log(err));
+// axios
+//   .get(`${BASE_URL}/pokemon?limit=100000&offset=0`)
+//   .then((res) => {
+//     const pokemonPromises = res.data.results;
+//     const pokemonCounts = res.data.results.length;
+//     // Part 2
+//     for (let i = 0; i < 3; i++) {
+//       let rand = Math.floor(Math.random() * pokemonCounts);
+//       threePokemonPromises.push(axios.get(pokemonPromises[rand].url));
+//     }
+//     // Part 3
+//     Promise.all(threePokemonPromises)
+//       .then((res) => {
+//         res.forEach((pokemon) => {
+//           axios.get(pokemon.data.species.url).then((res) => {
+//             let flavor_entry = res.data.flavor_text_entries.find(
+//               (entry) => entry.language.name === "en"
+//             );
+//             let name = res.data.name;
+//             console.log(`${name}: ${flavor_entry.flavor_text}`);
+//           });
+//         });
+//       })
+//       .catch((err) => console.log(err));
+//   })
+//   .catch((err) => console.log(err));
+
+// Async/Await
+const fetchApiData = async () => {
+  const res = await axios.get(`${BASE_URL}/pokemon?limit=100000&offset=0`);
+  const allPokemons = res.data.results;
+  let threePokemonPromises = [];
+  for (let i = 0; i < 3; i++) {
+    const rand = Math.floor(Math.random() * allPokemons.length);
+    threePokemonPromises.push(axios.get(allPokemons[rand].url));
+  }
+  const threePokemonRes = await Promise.all(threePokemonPromises);
+  for (let res of threePokemonRes) {
+    const name = res.data.name;
+    const { data } = await axios.get(res.data.species.url);
+    const entry = data.flavor_text_entries.find(
+      (el) => el.language.name === "en"
+    );
+    console.log(`${name}: ${entry.flavor_text}`);
+  }
+};
